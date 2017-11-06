6935d83bdac59efc9d7d8a696b9984988a933d61
popfisher <774232122@qq.com>
2017-11-06 22:10:53 +0800

    解析来去电号码

--- app/src/main/java/com/phone/listen/PhoneStateReceiver.java
+++ app/src/main/java/com/phone/listen/PhoneStateReceiver.java
@@ -14,12 +14,26 @@ public class PhoneStateReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+        String action = intent.getAction();
+        Log.d(PhoneListenService.TAG, "PhoneStateReceiver action: " + action);
+
+        String resultData = this.getResultData();
+        Log.d(PhoneListenService.TAG, "PhoneStateReceiver getResultData: " + resultData);
+
+        if (action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+            Log.d(PhoneListenService.TAG, "PhoneStateReceiver onReceive action: ACTION_NEW_OUTGOING_CALL");
             // 去电，可以用定时挂断
+            // 双卡的手机可能不走这个Action
+            String phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+            Log.d(PhoneListenService.TAG, "PhoneStateReceiver intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER): " + phoneNumber);
         } else {
-            //来电
+            // 来电去电都会走
             String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
             Log.d(PhoneListenService.TAG, "PhoneStateReceiver onReceive state: " + state);
+
+            String extraIncomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            Log.d(PhoneListenService.TAG, "PhoneStateReceiver onReceive extraIncomingNumber: " + extraIncomingNumber);
+
             if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
                 Log.d(PhoneListenService.TAG, "PhoneStateReceiver onReceive endCall");
                 HangUpTelephonyUtil.endCall(context);
